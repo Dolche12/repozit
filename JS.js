3387fbf9e1bdd830b68c51ca1f4af6cee3e16f50
Dolche12 <danilpopyl@yandex.ru>
2021-04-15 13:44:16 +0300

    Нижний регистр, создание массива

--- JS.js
+++ JS.js
@@ -1,8 +1,9 @@
-let money=200000, income='80000', addExpenses='2000, 6000, 8000', deposit=true, mission='500000', period=6;
+let money=200000, income='80000', addExpenses='2000, 6000, 8000', deposit=true, mission='50000', period=6;
 console.log(typeof money );
 console.log(typeof income );
 console.log(typeof deposit );
-console.log(typeof period );
 console.log(addExpenses.length);
 console.log("Период равен " + period + " месяцев");
 console.log("Цель заработать " + mission+ " рублей");
+console.log(addExpenses.toLowerCase());
+console.log(addExpenses.split(', '));
