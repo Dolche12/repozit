4b73819548bf7146ba5c252b6e9393f89590d510
Dolche12 <danilpopyl@yandex.ru>
2021-04-15 13:21:36 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,3 +1,8 @@
-let money, income='я думаю иначе', addExpenses, deposit, mission, period;
-alert('Самын лучшие курсы)');
-console.log('Правда ,лучший)');
+
+let money=200000, income='80000', addExpenses='2000, 6000, 8000', deposit=true, mission='500000', period=6;
+console.log(typeof money );
+console.log(typeof income );
+console.log(typeof deposit );
+console.log(addExpenses.length);
+console.log('Период равен ', period,' месяцев');
+console.log('Цель заработать ', mission,' рублей');
