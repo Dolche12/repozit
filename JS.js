67baa97ad2d4255caac2f070527121e88f934404
Dolche12 <danilpopyl@yandex.ru>
2021-04-15 14:05:53 +0300

    Update JS.js

--- JS.js
+++ JS.js
@@ -1,4 +1,9 @@
-let money=200000, income='80000', addExpenses='Интернет: 2000, Такси: 6000, Коммуналка: 8000', deposit=true, mission='50000', period=6;
+let money=200000, 
+income='80000', 
+addExpenses='Интернет: 2000, Такси: 6000, Коммуналка: 8000', 
+deposit=true, 
+mission='50000', 
+period=6;
 console.log(typeof money );
 console.log(typeof income );
 console.log(typeof deposit );
