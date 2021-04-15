db935c5517ee60be3a83eea319f20d8634777169
Dolche12 <danilpopyl@yandex.ru>
2021-04-15 13:11:26 +0300

    Вывели тип данных, Вывели к в консоль слова

--- JS.js
+++ JS.js
@@ -1,3 +1,7 @@
-let money=200000, income, addExpenses, deposit, mission, period;
-alert('Самын лучшие курсы)');
-console.log('Правда ,лучший)');
+let money=200000, income='80000', addExpenses='2000, 6000, 8000', deposit=true, mission='500000', period=6;
+console.log(typeof money );
+console.log(typeof income );
+console.log(typeof deposit );
+console.log(addExpense.length);
+console.log('Период равен ', period,' месяцев');
+console.log('Цель заработать ', mission,' рублей');
