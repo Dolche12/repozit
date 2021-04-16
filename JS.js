108cb6eb7da54427cbd39fc1ce128bb5a1f4cbf6
Dolche12 <danilpopyl@yandex.ru>
2021-04-16 15:23:33 +0300

    Сделали первые 6 пунктов домашнего задания .

--- JS.js
+++ JS.js
@@ -1,3 +1,13 @@
-let money, income, addExpenses, deposit, mission, period;
-alert('Самын лучшие курсы)');
-console.log('Правда ,лучший)');
+'use strict';
+
+let money=prompt('Ваш месячный доход?', '50000'), 
+addExpenses=prompt('Перечислите возможные расходы за рассчитываемый период через запятую','интернет, такси, коммуналка'),
+deposit=confirm('Есть ли у вас депозит в банке?(Ок-да,Отмена-нет)'),
+period=6,
+expenses1 = prompt('Введите обязательную статью расходов?', 'Транспорт'),
+amount1 = prompt('Во сколько это обойдется?'),
+expenses2 = prompt('Введите обязательную статью расходов?', 'Продукты'),
+amount2 = prompt('Во сколько это обойдется?'),
+income=prompt(''),
+budgetMonth = money - amount2 - amount1;
+console.log(budgetMonth);
