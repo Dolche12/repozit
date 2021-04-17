8e79e9859b969ed2326a108cd1a04cf6ff4fdc55
Dolche12 <danilpopyl@yandex.ru>
2021-04-17 20:41:37 +0300

    Исправлены ошибки

--- JS.js
+++ JS.js
@@ -11,6 +11,41 @@ amount2 = +prompt('Во сколько это обойдется?', 5000),
 mission = +prompt('Цель заработать :', '150000'),
 accumulatedMonth = getAccumulatedMonth(money, getExpensesMonth()), // Переменная, которая присваевает работу двух функций с callback(-ом)
 budgetDay = accumulatedMonth / 30;
+  /*
+          Функции
+  */
+          function getExpensesMonth (){           //Функция, которая возвращает сумму всех расходов за месяц
+            return amount1 + amount2;
+          }
+      
+          function getAccumulatedMonth (moneyMonth, expensesMonth){    //Функция, которая возвращает накопления за месяц
+            return moneyMonth - expensesMonth;
+          }
+      
+          function getTargetMonth (miss, callback){    //Функция, которая возвращает результат деления округляя в большую сторону
+            return Math.ceil(miss / callback);
+          }
+          
+          function showTypeOf (a){     //Функция, которая выводит в консоль тип данной переменной 
+            console.log(a, typeof(a));
+          }
+      
+          function getStatusIncome(){   //Функция, которая возвращает описание 
+            if (budgetDay === 1200) {
+                return console.log('У вас почти получилось попасть в группу с высокий уровень дохода! Постарайтесь лучше!');
+            } else if (budgetDay === 600) {
+                return console.log('У вас почти средний уровень дохода, но немного не хватает...');
+            } else if (budgetDay > 1200) {
+                return console.log('У вас высокий уровень дохода');
+            } else if (budgetDay < 1200 && budgetDay > 600) {
+                return console.log('У вас средний уровень дохода');
+            } else if (budgetDay < 0) {
+                return console.log('Что то пошло не так');
+            } else {
+                return console.log('К сожалению у вас уровень дохода ниже среднего');
+            }
+        }
+
   showTypeOf(money);               //
   showTypeOf(addExpenses);         //Вызов функции
   showTypeOf(deposit);             //
@@ -18,32 +53,4 @@ budgetDay = accumulatedMonth / 30;
   console.log('Возможные расходы: ' + addExpenses.split(', '));
   console.log('Цель будет достигнута за: ' + getTargetMonth(mission, getAccumulatedMonth(money, getExpensesMonth())) + ' месяцев(-a)');
   console.log('Бюджет на день : ' + Math.floor(budgetDay) + ' рублей');
-  console.log(getStatusIncome(budgetDay));
-
-  /*
-          Функции
-  */
-    function getExpensesMonth (){           //Функция, которая возвращает сумму всех расходов за месяц
-      return amount1 + amount2;
-    }
-
-    function getAccumulatedMonth (moneyMonth, expensesMonth){    //Функция, которая возвращает накопления за месяц
-      return moneyMonth - expensesMonth;
-    }
-
-    function getTargetMonth (miss, callback){    //Функция, которая возвращает результат деления округляя в большую сторону
-      return Math.ceil(miss / callback);
-    }
-    
-    function showTypeOf (a){     //Функция, которая выводит в консоль тип данной переменной 
-      console.log(a, typeof(a));
-    }
-
-    function getStatusIncome(budget){   //Функция, которая возвращает описание budget 
-    return(budget < 0) ? 'Что то пошло не так' :
-     (budget < 600) ? 'Что то пошло не так' :
-     (budget === 600) ? 'У вас почти средний уровень дохода, но немного не хватает...' :
-     (budget < 1200) ? 'У вас средний уровень дохода' :
-     (budget === 1200) ? 'У вас почти получилось попасть в группу с высокий уровень дохода! Постарайтесь лучше!' :
-     'У вас высокий уровень дохода';
-    }
+  getStatusIncome();
