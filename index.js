37effdaee97d260bdd0827d47b633f6c6af42a00
Dolche12 <danilpopyl@yandex.ru>
2021-04-30 23:06:48 +0300

    Исправление ошибок

--- index.js
+++ index.js
@@ -3,20 +3,25 @@ incomeAdd = document.getElementsByTagName('button')[0],
 expensesAdd = document.getElementsByTagName('button')[1],
 check = document.querySelector('#deposit-check'),
 additionalIncomeItem = document.querySelector('.additional_income-item'),
-budgetMonthValue = document.getElementsByClassName('.result-total budget_month-value'),
-budgetDayValue = document.getElementsByClassName('.result-total budget_day-value'),
-expensesMonthValue = document.getElementsByClassName('.result-total expenses_month-value'),
-additionalIncomeValue = document.getElementsByClassName('.result-total additional_income-value'),
-additionalExpensesValue = document.getElementsByClassName('.result-total additional_expenses-value'),
-incomePeriodValue = document.getElementsByClassName('.result-total income_period-value'),
-targetMonthValue = document.getElementsByClassName('.result-total target_month-value'),
+
+
+budgetMonthValue = document.getElementsByClassName('result-total budget_month-value')[0],
+
+
+budgetDayValue = document.getElementsByClassName('result-total budget_day-value')[0],
+expensesMonthValue = document.getElementsByClassName('result-total expenses_month-value')[0],
+additionalIncomeValue = document.getElementsByClassName('result-total additional_income-value')[0],
+additionalExpensesValue = document.getElementsByClassName('result-total additional_expenses-value')[0],
+incomePeriodValue = document.getElementsByClassName('result-total income_period-value')[0],
+targetMonthValue = document.getElementsByClassName('result-total target_month-value')[0],
 salaryAmount = document.querySelector('.salary-amount'),
 incomeItems = document.querySelector('.income-items'),
 expensesItems = document.querySelector('.expenses-items'),
 additionalExpensesItem = document.querySelector('.additional_expenses-item'),
 depositAmount = document.querySelector('.deposit-amount'),
 depositPercent = document.querySelector('.deposit-percent'),
-targetAmount = document.querySelector('.target-amount');
+targetAmount = document.querySelector('.target-amount'),
+periodSelect = document.querySelector('.period-select');
 
 console.log('buttonStart: ', buttonStart);
 console.log('incomeAdd: ', incomeAdd);
@@ -39,3 +44,4 @@ console.log('additionalExpensesItem: ', additionalExpensesItem);
 console.log('depositAmount: ', depositAmount);
 console.log('depositPercent: ', depositPercent);
 console.log('targetAmount: ', targetAmount);
+console.log('periodSelect: ', periodSelect);
